64746905c69bd863d489c2e8f0dab713912352c0
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-06-01 22:22:15 +0000

    populated grid div with the remaining countries

--- script/main.js
+++ script/main.js
@@ -8,6 +8,10 @@ $(document).ready(function () {
             console.log(countries);
             let def = $('#default');
             let defContent = "";
+
+            let grid = $('#grid');
+            let gridContent = "";
+
             countries.forEach(country => {
                 if(country.name === "Germany" || country.name === "United States of America" || country.name === "Brazil" || country.name === "Iceland"){
                     defContent = `
@@ -20,6 +24,17 @@ $(document).ready(function () {
                     </div>
                     `;
                     def.append(defContent);
+                }else{
+                    gridContent = `
+                    <div id="" class="countryDiv">
+                        <img class="flag" src="${country.flags.png}"/>
+                        <div id="" class="name"><strong>${country.name}</strong></div>
+                        <div id="" class="population"><b>Population </b> ${country.population}</div>
+                        <div id="" class="region"><b>Region: </b> ${country.region}</div>
+                        <div id="" class="capital"><b>Capital: </b> ${country.capital}</div>
+                    </div>
+                    `;
+                    grid.append(gridContent);
                 }
             });
         },
